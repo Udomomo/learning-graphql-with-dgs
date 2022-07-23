7ef168859482bca91513f1f2d3ff0bfbef076420
Udomomo <udomomo@gmail.com>
2022-07-23 18:27:45 +0900

    create dgs-server project

---
+++ dgs-server/src/main/kotlin/com/udomomo/learninggraphql/LearninggraphqlApplication.kt
@@ -0,0 +1,11 @@
+package com.udomomo.learninggraphql
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LearninggraphqlApplication
+
+fun main(args: Array<String>) {
+	runApplication<LearninggraphqlApplication>(*args)
+}
---
+++ dgs-server/src/test/kotlin/com/udomomo/learninggraphql/LearninggraphqlApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.udomomo.learninggraphql
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LearninggraphqlApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
