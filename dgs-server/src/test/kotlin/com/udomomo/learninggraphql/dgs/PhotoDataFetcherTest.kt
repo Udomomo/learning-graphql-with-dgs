586fd8e8642620063c94c791636962a13e3e92ad
Udomomo <udomomo@gmail.com>
2022-10-25 22:04:13 +0900

    fix PhotoDataFetcherTest

--- dgs-server/src/test/kotlin/com/udomomo/learninggraphql/dgs/PhotoDataFetcherTest.kt
+++ dgs-server/src/test/kotlin/com/udomomo/learninggraphql/dgs/PhotoDataFetcherTest.kt
@@ -2,15 +2,20 @@ package com.udomomo.learninggraphql.dgs
 
 import com.netflix.graphql.dgs.DgsQueryExecutor
 import com.netflix.graphql.dgs.autoconfig.DgsAutoConfiguration
+import com.udomomo.learninggraphql.service.UserService
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
 
 @SpringBootTest(classes = [DgsAutoConfiguration::class, PhotoDataFetcher::class])
 class PhotoDataFetcherTest {
     @Autowired
     lateinit var dgsQueryExecutor: DgsQueryExecutor
+
+    @MockBean
+    lateinit var userService: UserService
 
     @Test
     fun totalPhotos() {
